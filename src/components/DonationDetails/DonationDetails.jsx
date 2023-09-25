e5290347ff703b463ffe8f2b3afaccca891e8335
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-25 16:25:18 +0600

    donation page overlay added

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -2,9 +2,9 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const DonationDetails = () => {
     const donationData = useLoaderData();
-    const {id} = useParams(); 
+    const { id } = useParams();
     const idInt = parseInt(id);
-    const donation = donationData.find(donation=>donation.id === idInt);
+    const donation = donationData.find(donation => donation.id === idInt);
 
     // const handleApplyJob = () => {
     //     saveJobApplication(idInt);
@@ -12,13 +12,24 @@ const DonationDetails = () => {
     // }
     return (
         <div className="max-w-[1320px] mx-auto p-0 md:px-5 mb-20">
-           
-            <img className=" px-5 md:px-0 w-full md:min-w-full mx-auto max-h-[700px]  my-14" src={donation.picture} alt="" />
+
+          
+            <div className=" mx-5 md:px-0 relative">
+                <img
+                    className="w-full md:min-w-full mx-auto max-h-[700px] my-14"
+                    src={donation.picture}
+                    alt=""
+                />
+                <div
+                    className=" absolute bottom-0 left-0 w-full h-1/6 bg-[rgba(11,11,11,0.50)]"
+                ></div>
+            </div>
+
 
             <h1 className=" font-bold text-3xl text-center md:text-start md:text-[40px] mb-6">{donation.title}</h1>
             <p className="px-5 md:px-0 text-[16px] text-[#0B0B0BB2] text-justify">{donation.description}</p>
-            
-            
+
+
         </div>
     );
 };
