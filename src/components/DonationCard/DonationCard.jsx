2cee5daf591840166f22c07e824187a8c3c4012f
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-25 13:25:08 +0600

    home page cards' colors added

--- src/components/DonationCard/DonationCard.jsx
+++ src/components/DonationCard/DonationCard.jsx
@@ -6,11 +6,11 @@ const DonationCard = ({ donation }) => {
 
     
     return (
-        <div className={`card card-compact w-[312px]  shadow-xl bg-[${color_card_bg}]`}>
+        <div style={{backgroundColor: color_card_bg}} className={`card card-compact w-[312px]  shadow-xl `}>
             <figure><img src={picture} alt="" /></figure>
             <div className="card-body">
-                <span className={`p-1 text-[14px]  text-[${color_text_and_button_bg}] font-medium max-w-max bg-[${color_category_bg}]`}>{category}</span>
-                <h2 className={`card-title text-[${color_text_and_button_bg}]`}>{title}</h2>
+                <span style={{color: color_text_and_button_bg, backgroundColor: color_category_bg}} className={`py-1 px-2  text-[14px]   font-medium max-w-max `}>{category}</span>
+                <h2 style={{color: color_text_and_button_bg}} className={`card-title font-semibold`}>{title}</h2>
                 
                
             </div>
