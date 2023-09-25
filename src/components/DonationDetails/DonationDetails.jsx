bf09f21a2c8d2699ae1c68ea2e8ed73762a74d3f
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-25 17:13:41 +0600

    donation page button placed

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -14,17 +14,31 @@ const DonationDetails = () => {
         <div className="max-w-[1320px] mx-auto p-0 md:px-5 mb-20">
 
           
-            <div className=" mx-5 md:px-0 relative">
+            <div className="relative">
+            <div className=" mx-5 md:px-0 relative ">
                 <img
-                    className="w-full md:min-w-full mx-auto max-h-[700px] my-14"
+                    className="w-full md:min-w-full mx-auto max-h-[700px] my-14 "
                     src={donation.picture}
                     alt=""
                 />
+                 
+
+                
                 <div
-                    className=" absolute bottom-0 left-0 w-full h-1/6 bg-[rgba(11,11,11,0.50)]"
-                ></div>
+                    className=" absolute bottom-0 left-0 w-full h-[90px] md:h-[110px] bg-[rgba(11,11,11,0.50)]"
+                >
+                    
+           <div className="absolute bottom-1/4 left-8 md:left-10 lg:left-12">
+           <button style={{backgroundColor: donation.color_text_and_button_bg}} className="btn text-white text-xs md:text-[18px] border-none  font-semibold">Donate ${donation.price}</button>
+           </div>
+                </div>
+                
             </div>
 
+            </div>
+           
+
+           
 
             <h1 className=" font-bold text-3xl text-center md:text-start md:text-[40px] mb-6">{donation.title}</h1>
             <p className="px-5 md:px-0 text-[16px] text-[#0B0B0BB2] text-justify">{donation.description}</p>
