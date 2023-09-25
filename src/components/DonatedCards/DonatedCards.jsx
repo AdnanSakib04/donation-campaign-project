a25c344a6f47c3f538c9e588683ba00d0946f0ac
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-25 20:30:58 +0600

    donation page's cards done

--- src/components/DonatedCards/DonatedCards.jsx
+++ src/components/DonatedCards/DonatedCards.jsx
@@ -5,14 +5,14 @@ const DonatedCards = ({ donation }) => {
         <div>
 
 
-            <div style={{ backgroundColor: color_card_bg }} className="card card-side   max-w-[648px] ">
-                <figure  className=" max-w-[220px]  h-full"><img src={picture} alt="" /></figure>
+            <div style={{ backgroundColor: color_card_bg }} className="card card-side   md:min-w-[630px] ">
+                <figure  className="w-full md:max-w-[240px]  "><img className="h-full" src={picture} alt="" /></figure>
                 <div className="card-body">
                     <span style={{ color: color_text_and_button_bg, backgroundColor: color_category_bg }} className={`py-1 px-2  text-[14px]   font-medium max-w-max `}>{category}</span>
-                    <h2 className={`card-title text-2xl font-semibold`}>{title}</h2>
+                    <h2 className={`card-title text-xl md:text-2xl font-semibold`}>{title}</h2>
                     <p style={{ color: color_text_and_button_bg }} className=" text-[16px] font-semibold">${price}</p>
                     <div className="card-actions justify-start">
-                        <button style={{ backgroundColor: color_text_and_button_bg }} className="btn w-[140px] h-[40px] text-white text-xs md:text-[16px] border-none  font-semibold ">View Details</button>
+                        <button style={{ backgroundColor: color_text_and_button_bg }} className="btn w-[140px]  text-white text-xs md:text-[16px] border-none  font-semibold ">View Details</button>
                     </div>
                 </div>
             </div>
