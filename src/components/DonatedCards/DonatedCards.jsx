8421c33f369336968bba7a2e89fcb484fcaf2a48
AdnanSakib04 <navidadnan04@gmail.com>
2023-09-27 20:30:38 +0600

    data updated and card updated

--- src/components/DonatedCards/DonatedCards.jsx
+++ src/components/DonatedCards/DonatedCards.jsx
@@ -9,10 +9,10 @@ const DonatedCards = ({ donation }) => {
                 <figure  className="w-full md:max-w-[240px]  "><img className="h-full" src={picture} alt="" /></figure>
                 <div className="card-body">
                     <span style={{ color: color_text_and_button_bg, backgroundColor: color_category_bg }} className={`py-1 px-2  text-[14px]   font-medium max-w-max `}>{category}</span>
-                    <h2 className={`card-title text-xl md:text-2xl font-semibold`}>{title}</h2>
+                    <h2 className={`card-title text-xl md:text-[20px] font-semibold`}>{title}</h2>
                     <p style={{ color: color_text_and_button_bg }} className=" text-[16px] font-semibold">${price}</p>
                     <div className="card-actions justify-start">
-                        <button style={{ backgroundColor: color_text_and_button_bg }} className="btn w-[140px]  text-white text-xs md:text-[16px] border-none  font-semibold ">View Details</button>
+                        <button style={{ backgroundColor: color_text_and_button_bg }} className="btn w-[150px]  text-white text-xs md:text-[16px] border-none  font-semibold ">View Details</button>
                     </div>
                 </div>
             </div>
